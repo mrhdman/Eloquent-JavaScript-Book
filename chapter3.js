56be0ef3828b083803af9eb8ad369f006b0e411a
mrhdman <mattrhavins@gmail.com>
2019-05-08 15:38:24 -0400

    finished chap3 exercises with help from StackOverflow

--- chapter3.js
+++ chapter3.js
@@ -144,3 +144,93 @@ console.log(f(7))
 console.log(g(2, 7));
 console.log(h("hello".length));
 console.log(h2("test2".length));
+
+// Exercises
+
+
+// Minimum
+/*
+The previous chapter introduced the standard function Math.min that
+returns its smallest argument. We can build something like that now.
+Write a function min that takes two arguments and returns their minimum.
+*/
+
+const minimumMin = function(a, b) {
+    return Math.min(a, b);
+}
+console.log(minimumMin(83, 29));
+
+// Recursion
+
+/*
+We’ve seen that % (the remainder operator) can be used to test whether a number is even or odd by using % 2 to see whether it’s divisible by two. Here’s another way to define whether a positive whole number is even or odd:
+• Zero is even.
+• One is odd.
+• For any other number N, its evenness is the same as N - 2.
+Define a recursive function isEven corresponding to this description. The function should accept a single parameter (a positive, whole number) and return a Boolean.
+Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this?
+*/
+
+
+
+function isEven(number) {
+    if (number < 0) {
+        return isEven(-number);
+    } else if (number == 1) {
+        return false;
+    } else if (number == 0) {
+        return true;
+    } else {
+        return isEven(number - 2);
+    }
+}
+
+console.log(isEven(-1)); // false
+console.log(isEven(50)); // true
+console.log(isEven(75)); // false
+
+
+
+// Bean Counting
+
+/*
+You can get the Nth character, or letter, from a string by writing "string"[N]. The returned value will be a string containing only one character (for example, "b"). The first character has position 0, which causes the last one to be found at position string.length - 1. In other words, a two-character string has length 2, and its characters have positions 0 and 1.
+Write a function countBs that takes a string as its only argument and returns a number that indicates how many uppercase “B” characters there are in the string.
+Next, write a function called countChar that behaves like countBs, except it takes a second argument that indicates the character that is to be counted (rather than counting only uppercase “B” characters). Rewrite countBs to make use of this new function.
+*/
+
+function countBs(string) {
+    let count = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (string.charAt(i) === "B") {
+            count ++;
+        }
+    }
+    return (count);
+}
+
+console.log(countBs("BatmanBbBurr"));
+
+function countChar(string, character) {
+    let count = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (string.charAt(i) === character) {
+            count ++;
+        }
+    }
+    return (count);
+}
+
+console.log(countChar("Matthew", "t")); // return 2
+
+function countBs1(string, character) {
+    let count = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (string.charAt(i) === character) {
+            count++;
+        }
+    }
+    return (count);
+}
+
+console.log(countBs1("hellohelloHELLOHEllOHElLO", "l" ));
